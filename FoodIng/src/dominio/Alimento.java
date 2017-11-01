bdda84d21e59b6fc82adcca87973cdf4fb60fe35
Martin Rezy <m_rezy@hotmail.com>
2017-11-01 11:44:01 -0300

    Se agrega funcion para agregar un alimento.

--- FoodIng/src/dominio/Alimento.java
+++ FoodIng/src/dominio/Alimento.java
@@ -12,10 +12,40 @@ import java.util.ArrayList;
  * @author gonzalostrauss
  */
 public class Alimento {
+
     private String nombre;
     private String familia;
-    int cantidades;
     //private Familia familia;
     //private Diccionario<String,String> nutrientes;
     private ArrayList<String> nutrientes;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFamilia() {
+        return familia;
+    }
+
+    public void setFamilia(String familia) {
+        this.familia = familia;
+    }
+
+    public ArrayList<String> getNutrientes() {
+        return nutrientes;
+    }
+
+    public void agregarNutriente(String nutriente) {
+        nutrientes.add(nutriente);
+    }
+
+    public Alimento(String nombre, String familia) {
+        this.setNombre(nombre);
+        this.setFamilia(familia);
+        this.nutrientes = new ArrayList<String>();
+    }
 }
--- FoodIng/src/dominio/Sistema.java
+++ FoodIng/src/dominio/Sistema.java
@@ -1,4 +1,5 @@
 package dominio;
+
 import java.io.Serializable;
 import java.util.*;
 
@@ -6,6 +7,7 @@ public class Sistema implements Serializable {
 
     private ArrayList<Usuario> listaUsuarios;
     private ArrayList<Profesional> listaProfesionales;
+    private ArrayList<Alimento> listaAlimentos;
 
     public ArrayList<Usuario> getListaUsuarios() {
         return listaUsuarios;
@@ -15,9 +17,14 @@ public class Sistema implements Serializable {
         return listaProfesionales;
     }
 
+    public ArrayList<Alimento> getListaAlimentos() {
+        return listaAlimentos;
+    }
+
     public Sistema() {
         listaUsuarios = new ArrayList<Usuario>();
         listaProfesionales = new ArrayList<Profesional>();
+        listaAlimentos = new ArrayList<Alimento>();
     }
 
     public void agregarUsuario(String nombre, String apellidos, String nacionalidad, Date nacimiento, String pathPerfil) {
@@ -25,8 +32,14 @@ public class Sistema implements Serializable {
         this.getListaUsuarios().add(nuevoUsuario);
     }
 
-    public void agregarProfesional(String nombre, String apellidos, String nacionalidad, Date nacimiento, String pathPerfil, String titulo,String paisTitulo,Date fechaGrad) {
-        Profesional nuevoProfesional = new Profesional(nombre,apellidos,nacionalidad,nacimiento,pathPerfil,titulo,fechaGrad,paisTitulo);
+    public void agregarProfesional(String nombre, String apellidos, String nacionalidad, Date nacimiento, String pathPerfil, String titulo, String paisTitulo, Date fechaGrad) {
+        Profesional nuevoProfesional = new Profesional(nombre, apellidos, nacionalidad, nacimiento, pathPerfil, titulo, fechaGrad, paisTitulo);
         this.getListaProfesionales().add(nuevoProfesional);
+    }
+
+    public void agregarAlimento(String nombre, String familia, ArrayList<String> nutrientes) {
+        Alimento nuevoAlimento = new Alimento(nombre, familia);
+        this.getListaAlimentos().add(nuevoAlimento);
+
     }
 }
