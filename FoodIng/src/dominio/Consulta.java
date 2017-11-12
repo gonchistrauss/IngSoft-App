bdcba545aa934149230a60daae7212bba5ad4936
Martin Rezy <m_rezy@hotmail.com>
2017-11-12 11:58:36 -0300

    Se agregan toString para consulta y para incidencia

--- FoodIng/src/dominio/Consulta.java
+++ FoodIng/src/dominio/Consulta.java
@@ -66,4 +66,14 @@ public class Consulta {
         this.setCategoria(unaCategoria);
         listaIncidencias = new ArrayList<Incidencia>();
     }
+    
+       //Metodo toString
+    @Override
+    public String toString() {
+       String consulta = "";
+       for(Incidencia incidencia : this.getListaIncidencias()){
+           consulta += incidencia.toString() + "\n *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-\n";
+       }
+       return consulta;
+    }
 }
